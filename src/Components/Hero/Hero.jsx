d6e1998308db6980a73a30406c450d51e0e41aa2
Navoda Rajapakshe <nawoda1112rajapakshe@gmail.com>
2024-10-03 00:59:40 +0530

    shop page done

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,16 +9,18 @@ const Hero = () => {
     <div className='hero'>
       <div className="hero-left">
         <h2>NEW ARRIVALS ONLY</h2>
-        <div className="hand-hand-icon">
+        <div>
+        <div className="hero-hand-icon">
             <p>New</p>
             <img src={hand_icon} alt=''/>
         </div>
-        <p>collection</p>
-        <p>for everyone</p>
-      </div>
+        <p>Collection</p>
+        <p>For Everyone</p>
+        </div>
       <div className="hero-latest-btn">
         <div>Latest Collection</div>
         <img src={arrow_icon} alt=''/>
+      </div>
       </div>
       <div className="hero-right">
         <img src={hero_image} alt=''/>
