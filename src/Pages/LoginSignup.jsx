379dafc293cc35b5388a004d5cd78fb1422850b3
Navoda-Rajapakshe01 <rajapakshermnn.22@uom.lk>
2024-10-12 23:19:31 +0530

    updates related to product display

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './CSS/loginsignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className='loginsignup'> 
+    <div className="loginsignup-container">
+      <h1>Sign Up</h1>
+      <div className="loginsingup-fields">
+        <input type="text" placeholder='Your Name' />
+        <input type="email" placeholder='Email'/>
+        <input type="password" placeholder='Password' />
+      </div>
+      <div className="loginsignup-agree">
+        <input type="checkbox" />
+        <p>By continuing, i agree to the terms of use & privacy</p>
+      </div>
+      <button>Continue</button>
+      <p className='loginsignup-login'>Already have an account? <span>Login Here</span></p>
+    </div>
     </div>
   )
 }
